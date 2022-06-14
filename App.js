2e637a9cbdd4ca27e04b7932d3c78874cb4ae403
Poorvi52 <74486589+Poorvi52@users.noreply.github.com>
2022-06-14 12:31:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import { render } from "react-dom";
+import { StyleSheet, Text, View } from 'react-native';
+import PickImage from "./screens/camera"
+
+export default class App extends React.Component {
+  render() {
+    return <PickImage/> 
+  }
+
+ 
+}
+
